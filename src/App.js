9af06e4be11ab0d1571fecb6dc947826680dead6
inoue-ryoki <mth.1021@icloud.com>
2023-04-15 11:00:59 +0900

    stateまで実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './styles.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [incompleteTodos, setIncompleteTodos] = useState(['ああああ', 'いいいい']);
+  const [completeTodos, setcompleteTodos] = useState(['うううう', 'ええええ']);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <>
+          <div className="input-area">
+            <input placeholder='TODOを入力' />
+            <button>追加</button>
+          </div>
+          <div className="incomplete-area">
+            <p className='title'>未完了のTODO</p>
+            <ul>
+              {incompleteTodos.map((todo) => {
+                return (
+                  <div key={todo} className='list-row'>
+                    <li>{todo}</li>
+                    <button>完了</button>
+                    <button>削除</button>
+
+                  </div>
+
+                )
+              })}
+            </ul>
+
+          </div>
+
+          <div className="complete-area">
+            <p className='title'>完了のTODO</p>
+            <ul>
+              {completeTodos.map((todo) => {
+                return (
+                  <div key={todo} className='list-row'>
+                    <li>{todo}</li>
+                    <button>戻す</button>
+
+                  </div>
+
+                )
+              })}
+
+            </ul>
+
+          </div>
+
+          <div>
+
+          </div>
+        </>
       </header>
+
     </div>
   );
 }
