05467e4fe16331d4c5801ef5f4a2e79dbb0410c8
inoue-ryoki <mth.1021@icloud.com>
2023-04-16 11:36:42 +0900

    削除機能実装

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ function App() {
     setTodoText('');
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,12 +32,12 @@ function App() {
           <div className="incomplete-area">
             <p className='title'>未完了のTODO</p>
             <ul>
-              {incompleteTodos.map((todo) => {
+              {incompleteTodos.map((todo, index) => {
                 return (
                   <div key={todo} className='list-row'>
                     <li>{todo}</li>
                     <button>完了</button>
-                    <button>削除</button>
+                    <button onClick={() => onClickDelete(index)}>削除</button>
 
                   </div>
 
